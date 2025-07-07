46319b2fcabd35b73d2305a674cb9fe28e95d64d
Fenrir Eru <139980729+Ramanilovatar@users.noreply.github.com>
2025-07-07 11:47:21 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var loader = document.getElementById('loading');
+var Main = document.getElementById('main');
+
+window.addEventListener('load', function () {
+    loader.style.display = 'none';
+});
